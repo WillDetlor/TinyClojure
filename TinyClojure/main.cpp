8cf10afb6105f980aa7794e1a43d0e40eed41dbc
Duncan Steele <steeleduncan@hotmail.com>
2012-10-20 17:54:44 +0200

    Initial Commit

---
+++ TinyClojure/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  TinyClojure
+//
+//  Created by Duncan Steele on 20/10/2012.
+//  Copyright (c) 2012 Slide to code. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
