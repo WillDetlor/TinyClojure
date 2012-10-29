c51680e67c6a8efc4285abcad18b9618fefb1f57
Duncan Steele <steeleduncan@hotmail.com>
2012-10-29 08:09:03 +0100

    test fixes

--- tests/trip.clj
+++ tests/trip.clj
@@ -6,10 +6,10 @@
 
 ; basic test that defs remain within a scope
 (def a 10)
-print (- a 10)
+(print (- a 10))
 (do
     (def a 10)
-    print (- a 10))
+    (print (- a 10)))
 
 ; make sure state is being captured
 (def con 12)
